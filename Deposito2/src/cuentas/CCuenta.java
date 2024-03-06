ddc7ccfe44a9aaed1ddb12ce1eb052367fa16f2c
Ivan <idelacruzvelasco@gmail.com>
2024-03-06 17:01:00 +0100

    Encapsular atributos CCuenta

--- Deposito2/src/cuentas/CCuenta.java
+++ Deposito2/src/cuentas/CCuenta.java
@@ -5,7 +5,7 @@ public class CCuenta {
     private String nombre;
     private String cuenta;
     private double saldo;
-    private double tipoInterés;
+    private double tipoInteres;
 
     public CCuenta()
     {
@@ -13,21 +13,21 @@ public class CCuenta {
 
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
-        nombre =nom;
-        cuenta=cue;
-        saldo=sal;
+        setNombre(nom);
+        setCuenta(cue);
+        setSaldo(sal);
     }
 
     public double estado()
     {
-        return saldo;
+        return getSaldo();
     }
 
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
             throw new Exception("No se puede ingresar una cantidad negativa");
-        saldo = saldo + cantidad;
+        setSaldo(getSaldo() + cantidad);
     }
 
     public void retirar(double cantidad) throws Exception
@@ -36,6 +36,38 @@ public class CCuenta {
             throw new Exception ("No se puede retirar una cantidad negativa");
         if (estado()< cantidad)
             throw new Exception ("No se hay suficiente saldo");
-        saldo = saldo - cantidad;
+        setSaldo(getSaldo() - cantidad);
     }
+
+	String getNombre() {
+		return nombre;
+	}
+
+	void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	String getCuenta() {
+		return cuenta;
+	}
+
+	void setCuenta(String cuenta) {
+		this.cuenta = cuenta;
+	}
+
+	double getSaldo() {
+		return saldo;
+	}
+
+	void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+	double getTipoInterés() {
+		return tipoInteres;
+	}
+
+	void setTipoInterés(double tipoInterés) {
+		this.tipoInteres = tipoInterés;
+	}
 }
